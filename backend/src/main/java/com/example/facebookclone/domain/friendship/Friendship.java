016a609dd5a923540c5c79e880c6b29bf1786b5d
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2026-02-03 09:26:43 +0100

    fill Friendship entity

--- backend/src/main/java/com/example/facebookclone/domain/friendship/Friendship.java
+++ backend/src/main/java/com/example/facebookclone/domain/friendship/Friendship.java
@@ -1,4 +1,59 @@
 package com.example.facebookclone.domain.friendship;
 
+import com.example.facebookclone.domain.user.User;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(
+        name = "friendships",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"requester_id", "addressee_id"})
+)
 public class Friendship {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "requester_id")
+    private User requester;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "addressee_id")
+    private User addressee;
+
+    @Enumerated(EnumType.STRING)
+    private FriendshipStatus status;
+
+    private Instant createdAd = Instant.now();
+
+    protected Friendship() {}
+
+    public Friendship(User requester, User addressee) {
+        this.requester = requester;
+        this.addressee = addressee;
+        this.status = FriendshipStatus.PENDING;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getRequester() {
+        return requester;
+    }
+
+    public User getAddressee() {
+        return addressee;
+    }
+
+    public FriendshipStatus getStatus() {
+        return status;
+    }
+
+    public Instant getCreatedAd() {
+        return createdAd;
+    }
 }
