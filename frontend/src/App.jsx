61a858c51db1c1c836a870bc379355d65c19973e
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2026-02-16 20:13:46 +0100

    feat: protect app with auth guard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
+import { useAuth } from "./auth/AuthContext";
 import Header from "./components/Header";
 import Feed from "./pages/Feed";
+import Login from "./pages/Login.jsx";
 
 export default function App() {
-    return (
-        <>
-            <Header />
-            <Feed />
-        </>
-    );
+    const { token } = useAuth();
+    if (!token) return <Login />
+    return <Feed />
 }
