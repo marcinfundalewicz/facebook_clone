31fe47786bba57800a4198397761025dd1626814
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2026-02-17 18:31:07 +0100

    feat(api): add getPosts endpoint

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,3 +3,7 @@ import client from "./client";
 export function login(email, password) {
     return client.post("/auth/login", { email, password });
 }
+
+export function getPosts() {
+    return client.get("/api/posts");
+}
