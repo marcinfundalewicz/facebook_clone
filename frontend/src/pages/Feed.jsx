352b53e0d94e221efd8c38c00ea7a23b0aaf9e4e
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2026-03-03 20:36:13 +0100

    add loading state to feed"

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -5,14 +5,18 @@ import { getPosts, createPost } from "../api/api.js";
 
 export default function Feed() {
     const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function loadPosts() {
             try {
+                setLoading(true);
                 const res = await getPosts();
                 setPosts(res.data.content);
             } catch (err) {
                 console.error("Failed to load posts", err);
+            } finally {
+                setLoading(false);
             }
         }
 
@@ -27,6 +31,10 @@ export default function Feed() {
         } catch (err) {
             console.error("Create post failed", err);
         }
+    }
+
+    if(loading) {
+        return <p>Loading posts...</p>;
     }
 
     return (
