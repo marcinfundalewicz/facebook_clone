ac543d03446857a0a31cb7887b3b8af80b99cce2
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2026-03-13 19:36:37 +0100

    add react router and profile page routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useAuth } from "./auth/AuthContext";
+
 import Feed from "./pages/Feed";
-import Login from "./pages/Login.jsx";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+
 import Navbar from "./components/Navbar";
 
 export default function App() {
   const { token, logout } = useAuth();
+
   if (!token) return <Login />;
+
   return (
-    <>
+    <BrowserRouter>
       <Navbar onLogout={logout} />
-      <Feed />
-    </>
+
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
