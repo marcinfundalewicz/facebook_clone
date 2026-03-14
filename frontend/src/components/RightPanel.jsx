810c6844269ab02cf6572afe24d95351d94f776e
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2026-03-14 12:12:06 +0100

    improve right panel suggestions list

--- frontend/src/components/RightPanel.jsx
+++ frontend/src/components/RightPanel.jsx
@@ -1,24 +1,18 @@
 import { getAvatar } from "../utils/avatar";
 
 export default function RightPanel() {
-  const users = ["John", "Anna", "Michael"];
   const suggestions = ["Emma", "Lucas", "Sophia", "Michael"];
 
   return (
     <div className="right-panel">
       <div className="right-card">
         <h4>Who to follow</h4>
+
         {suggestions.map((u) => (
           <div className="follow-item" key={u}>
-            <img src={getAvatar(u)} className="avatar small" />
-            <span>{u}</span>
-          </div>
-        ))}
-        {users.map((user) => (
-          <div className="follow-item" key={user}>
-            <img className="avatar small" src={getAvatar(user)} alt={user} />
+            <img src={getAvatar(u)} className="avatar small" alt={u} />
 
-            <span>{user}</span>
+            <span>{u}</span>
           </div>
         ))}
       </div>
